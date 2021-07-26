22c845c5a98114cd13478d1238cfc1c94447ceb0
Meg McGregor <megmcgregor1@gmail.com>
2021-07-26 11:26:12 -0700

    initial file and auth setup

---
+++ SitterShare/Models/Babysitter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SitterShare.Models
+{
+    public class Babysitter
+    {
+        public int Id { get; set; }
+        public string SitterFirebaseUid { get; set; }
+        public int UserTypeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool isMinor { get; set; }
+        public int Zipcode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set }
+        public bool isCprCertified { get; set; }
+        public bool hasDriversLisence { get; set; }
+        public bool hasTransportation { get; set; }
+        public bool hasInfantExperience { get; set; }
+
+    }
+}
